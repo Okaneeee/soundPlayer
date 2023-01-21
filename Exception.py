6ebaeffe658d23a5638f0d343c4fec1790e0413d
Okane <anatole.giniaux@sfr.fr>
2023-01-21 19:01:49 +0100

    Fixed docstrings

--- Exception.py
+++ Exception.py
@@ -4,9 +4,9 @@ class FileExtensionException(Exception):
     """Exception raised for errors in the file extension
 
     Attributes:
-        file_type (str, optional): 
-        message (str, optional): 
+        file_type (str, optional): File extension of the file that caused the error\n
+        message (str, optional): Error message to send, Defaults: File must be a .wav
     """
     def __init__(self: Self, file_type: str|None = None, message: str = "File must be a .wav") -> None:
-        self.message = message + ' (Current file extension:' + file_type + ')' if file_type else message
+        self.message = message + ' (Current file extension: .' + file_type + ')' if file_type else message
         super().__init__(self.message)
