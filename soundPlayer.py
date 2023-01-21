8552f49515a4103fa471d24f1f6cafb63e88300c
Okane <anatole.giniaux@sfr.fr>
2023-01-21 21:09:30 +0100

    Readability, edited tests

--- soundPlayer.py
+++ soundPlayer.py
@@ -5,6 +5,9 @@ from typing_extensions import Self
 from Exception import FileExtensionException
 
 class SoundPlayer:
+    """
+    Python class to play .wav file 
+    """
     def __init__(self: Self, file_name: str):
         extension : list[str] = file_name.split(".")
         if (extension != ".wav"):
@@ -16,7 +19,7 @@ class SoundPlayer:
         self.p: PyAudio = PyAudio()
         self.stream: Stream
 
-    def play(self: Self):
+    def play(self: Self) -> None:
         """
         Method that starts the audio stream and launches a separate thread that reads the data from the wave file
         and writes it to the stream using stream.write(). This way the method will start playing
@@ -24,16 +27,16 @@ class SoundPlayer:
         """
         # Opening the audio stream
         self.stream: Stream = self.p.open(
-            format=self.p.get_format_from_width(self.wf.getsampwidth()),
-            channels=self.wf.getnchannels(),
-            rate=self.wf.getframerate(),
-            output=True
+            format = self.p.get_format_from_width(self.wf.getsampwidth()),
+            channels = self.wf.getnchannels(),
+            rate = self.wf.getframerate(),
+            output = True
         )
         self.stream.start_stream()
         self.thread: Thread = Thread(target=self._play_sound)
         self.thread.start()
 
-    def _play_sound(self: Self):
+    def _play_sound(self: Self) -> None:
         """
         Method that reads the data from the wave file and writes it to the audio stream using a while loop
         until there's no more data. It then stops the stream.
@@ -45,7 +48,7 @@ class SoundPlayer:
             self.data = self.wf.readframes(1024)
         self.stream.stop_stream()
 
-    def close(self: Self):
+    def close(self: Self) -> None:
         """
         Method used to close the audio stream and terminate the pyaudio object. 
         Should be called when the sound is finished playing to free up resources that were allocated.
@@ -57,7 +60,11 @@ if __name__ == '__main__':
     from tkinter import Tk
     w: Tk = Tk()
     
-    player = SoundPlayer("./medias/Kiki.mp3")
+    # mp3: str = "./medias/Rollin_at_5.mp3"
+    # player = SoundPlayer(mp3) # Should raise an exception
+
+    wav: str = "./medias/Early_Riser.wav"
+    player = SoundPlayer(wav) # Should work
     player.play()
 
     w.mainloop()
